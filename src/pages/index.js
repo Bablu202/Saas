31f0e7f787d828446c5a7111a723292696187f08
aware <aware9758@gmail.com>
2023-08-23 14:34:32 +0200

    all

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,34 @@
-export default function HomePage() {}
+import Image from "next/image";
+import Link from "next/link";
+import hero from "../../public/assets/hero.png";
+export default function HomePage() {
+  return (
+    <div className="grid-halves h-screen-navbar">
+      <div className="bg-teal border-right">
+        <div className="column-padding">
+          <div className="tablet-centred">
+            <div className="content-grid home-hero">
+              <h1>
+                BIM 3D Objects <br />
+                that we need most
+              </h1>
+              <p className="section-subtitle">
+                with all parameters editable ready
+              </p>
+              <Link href="/products" className="large-button">
+                <div className="large-button-text">Explore Objects</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-salmon">
+        <div className="column-padding centred">
+          <div className="callout-wrap">
+            <Image src={hero} className="callout-image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
