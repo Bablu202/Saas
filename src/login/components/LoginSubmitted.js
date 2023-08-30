9edf3bceb80e46bb8137917b8917c8c9262b6bc8
aware <aware9758@gmail.com>
2023-08-30 15:32:58 +0200

    30.08

--- src/login/components/LoginSubmitted.js
+++ src/login/components/LoginSubmitted.js
@@ -1,1 +1,8 @@
-export default function LoginSubmitted() {}
+export default function LoginSubmitted({ submitted }) {
+  return (
+    <div className="content-grid home-hero">
+      <h1>A magic Link sent to your Mail!</h1>
+      <p>{`check your email  ${submitted} to login.`}</p>
+    </div>
+  );
+}
